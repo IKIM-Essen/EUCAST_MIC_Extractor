92b929fa52c1990814a743dc78e4013fec5affcf
Julian-W98 <82864923+Julian-W98@users.noreply.github.com>
2024-12-30 16:33:42 +0000

    chore: update copyright preamble

--- table_handler.py
+++ table_handler.py
@@ -1,3 +1,8 @@
+# Copyright 2025 Julian Welling Institute for AI in Medicine (IKIM) 
+# Licensed under the MIT License
+# This file may not be copied, modified, or distributed
+# except according to those terms.
+
 from openpyxl import load_workbook
 import pandas as pd
 import re
