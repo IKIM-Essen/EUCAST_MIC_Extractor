fb5913d1c103a54e1700407ae133dff06b878dd7
Julian-W98 <julianwellingwork@gmail.com>
2024-12-30 16:13:55 +0100

    Save as json

--- main.py
+++ main.py
@@ -6,31 +6,32 @@
 import pandas as pd
 from openpyxl import load_workbook
 import re
+from json import loads, dump
 
 def load_raw_workbook(path):
-    # Load
+    # load
     wb = load_workbook(path, rich_text=True)
     sheet_dfs = {}
     for sheet in wb.worksheets:
         data = []
-        # Clean
+        # clean
         for row in sheet.iter_rows():
             row_data = []
             for cell in row:
                 if isinstance(cell.value, str):
                     cleaned_value = re.sub(r'<.*?>', '', cell.value)    # Remove HTML-like tags
-                    cleaned_value = re.sub(r"[\']", "", cleaned_value)
+                    cleaned_value = re.sub(r"[\']", "", cleaned_value)  # Remove '
                     row_data.append(cleaned_value)
                 elif isinstance(cell.value, list):
                     cleaned_value = cell.value[0]
-                    if cleaned_value == "":
+                    if isinstance(cleaned_value, str) and cleaned_value == "":
                         cleaned_value = cell.value[1]
                     row_data.append(cleaned_value)
                 else:
                     row_data.append(cell.value)  # Append non-string values as-is
             data.append(row_data)  # Append the processed row
         
-        # Store
+        # store
         df = pd.DataFrame(data)
         sheet_dfs[sheet.title] = df
 
@@ -50,12 +51,8 @@ def set_MIC_index(input_table):
         entry = input_table.at[idx, "MIC_Index"]
         if entry == 0:  # Found the 'MIC breakpoint'
             inside_mic = True
-            next_is_header = True
             continue
         if inside_mic:
-            if next_is_header:
-                next_is_header = False
-                continue
             if entry == -1:  # No 'MIC breakpoint'
                 input_table.at[idx, "MIC_Index"] = 1.0
             if pd.isna(input_table.at[idx, "MIC_Index"]): # Reset when a row is NaN (indicating end of MIC section)
@@ -74,7 +71,8 @@ def shrink_clean_table(input_table):
                         new_row = {"Name" : input_table[input_table.columns[0]][idx], "S <=" : input_table[input_table.columns[1]][idx], "R >" : input_table[input_table.columns[2]][idx]}
                         output_table = pd.concat([output_table, pd.DataFrame([new_row])], ignore_index=True)
                         
-        # Clean table     
+        # Clean table
+        output_table = output_table.dropna(how='any') 
         output_table['Name'] = output_table['Name'].map(lambda x: re.sub(r",", "", x) if isinstance(x, str) else x)
         output_table['Name'] = output_table['Name'].map(lambda x: re.sub(r"\n", "", x) if isinstance(x, str) else x)
         output_table['S <='] = output_table['S <='].map(lambda x: re.sub(r"[()]", "", x) if isinstance(x, str) else x)
@@ -82,10 +80,14 @@ def shrink_clean_table(input_table):
 
         return output_table
 
-raw_workbook = load_raw_workbook('resources/v_14.0_Breakpoint_Tables.xlsx')
-raw_table = raw_workbook["Pseudomonas"]
+def save_as_json(input_table, path):
+    vanilla_json = input_table.to_json(orient="records")
+    parsed_json = loads(vanilla_json)
+    with open(path, "w", encoding="utf-8") as json_file: dump(parsed_json, json_file, ensure_ascii=False, indent=4)
+     
+
+raw_workbook = load_raw_workbook('resources/v_12.0_Breakpoint_Tables.xlsx')
+raw_table = raw_workbook["Streptococcus A,B,C,G"]
 mic_table = set_MIC_index(raw_table)
 shrunk_table = shrink_clean_table(mic_table)
-pd.set_option('display.max_columns', 300)
-pd.set_option('display.max_rows', 300)
-print(shrunk_table)
+save_as_json(shrunk_table, "output/shrunk_table.json")
